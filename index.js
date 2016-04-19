d86a372a93bb7c1dfe55e5d4c35393377c7c853b
Brady Wied <brady@bswtechconsulting.com>
2016-04-19 16:22:14 -0600

    this was the wrong way to do this

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 'use strict'
 
-const loaderUtils = require('loader-utils')
-
-function transformLine(customLoader, line) {
-  const replaceString = customLoader && customLoader.loader ? `${customLoader.loader}!$1` : '$1'
-  return line.replace(/\/\/\= require (.*)/, `require("${replaceString}");`)
+function transformLine(line) {
+  return line.replace(/\/\/\= require (.*)/, 'require("$1");')
 }
 
 module.exports = function(source) {
   const lines = source.split('\n')
 
-  const customLoader = typeof this.query !== 'undefined' ? loaderUtils.parseQuery(this.query) : null
-
-  return lines.map(line => transformLine(customLoader, line)).join('\n')
+  return lines.map(transformLine).join('\n')
 }
